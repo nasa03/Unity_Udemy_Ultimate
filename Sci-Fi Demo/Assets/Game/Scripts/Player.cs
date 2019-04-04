070a3162275f72ec2f6acfb951642d612e530e5d
Iribe18 <manuel.iribe27@gmail.com>
2019-04-03 19:21:11 -0700

    + Adding NavMesh.asset * Adding RayCast to check when the Crosshair is hitting a collider.

--- Sci-Fi Demo/Assets/Game/Scripts/Player.cs
+++ Sci-Fi Demo/Assets/Game/Scripts/Player.cs
@@ -8,15 +8,36 @@ public class Player : MonoBehaviour
     private float Speed = 3.5f;
     private float Gravity = 9.81f;
     private CharacterController TheCharacterController;
+    
     // Start is called before the first frame update
     void Start()
     {
         TheCharacterController = GetComponent<CharacterController>();
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray rayOrigin = Camera.main.ViewportPointToRay(new Vector3(0.5f,0.5f,0f));
+            RaycastHit hitInfo;
+            if(Physics.Raycast(rayOrigin,out hitInfo))
+            {
+                //if(!hitInfo.transform.name.Equals("Player"))
+                {
+                    Debug.Log("Hit: " + hitInfo.transform.name);
+                }
+                
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.Escape) && !Cursor.visible)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
         Movement();
     }
 
